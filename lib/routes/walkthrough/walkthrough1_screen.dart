dc21897a234cb33d0853b1f5901fb9f5adac831c
viiducc <viduct2000@gmail.com>
2021-07-24 10:50:11 +0700

    Update Walkthrough_Screen1

--- lib/routes/walkthrough/walkthrough1_screen.dart
+++ lib/routes/walkthrough/walkthrough1_screen.dart
@@ -5,18 +5,103 @@ class WalkThrough1 extends StatelessWidget {
   const WalkThrough1();
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
-        body: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset('assets/images/walkthrough1/undraw.png'),
-          const Text('Wellcome to aking'),
-          const SizedBox(height: 20),
-          const Text('Whats going to happen tomorrow?'),
-          Image.asset('assets/images/walkthrough1/footer.png')
-        ],
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(35, 90, 35, 30),
+                child: Image.asset('assets/images/walkthrough1/undraw.png'),
+              ),
+              Container(
+                child: Column(
+                  children: const [
+                    Text(
+                      'Wellcome to aking',
+                      style: TextStyle(
+                        color: Color(0xFF313131),
+                        fontFamily: 'AvenirNextRoundedPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Text(
+                      'Whats going to happen tomorrow?',
+                      style: TextStyle(
+                        color: Color(0xFF313131),
+                        fontFamily: 'AvenirNextRoundedPro',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset('assets/images/walkthrough1/Pagination.png'),
+              Stack(
+                children: [
+                  Container(
+                    width: size.width,
+                    child: Image.asset(
+                      'assets/images/walkthrough1/footer.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    left: (size.width - 293) / 2,
+                    top: 97,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(293, 48),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        primary: Color(0xFFFFFFFF), // background
+                        onPrimary: Color(0xFF313131), // foreground
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Get Started',
+                        style: TextStyle(
+                          color: Color(0xFF313131),
+                          fontFamily: 'AvenirNextRoundedPro',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: (size.width - 52) / 2,
+                    top: 177,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(52, 22),
+                        //primary: Colors.white,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Color(0xFFFFFFFF),
+                          fontFamily: 'AvenirNextRoundedPro',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-    )));
+    );
   }
 }
--- lib/routes/walkthrough/walkthrough_screen.dart
+++ lib/routes/walkthrough/walkthrough_screen.dart
@@ -5,22 +5,26 @@ class Walkthrough extends StatelessWidget {
   const Walkthrough();
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Image.asset('assets/images/walkthrouh/fill1.png'),
-        Container(
-          margin: const EdgeInsets.all(20),
-          child: const Text('aking',
-              style: TextStyle(
-                color: Color(0xFF010101),
-                fontFamily: 'AvenirNextRoundedPro',
-                fontWeight: FontWeight.w900,
-                fontSize: 48,
-              )),
-        )
-      ],
-    )));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/walkthrouh/fill1.png'),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: const Text(
+                'aking',
+                style: TextStyle(
+                  color: Color(0xFF010101),
+                  fontFamily: 'AvenirNextRoundedPro',
+                  fontWeight: FontWeight.w900,
+                  fontSize: 48,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
